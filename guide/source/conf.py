64150f5a16b7ea406e73efced553fc94bd34b868
Noel Merket <noel.merket@nrel.gov>
2014-09-03 16:20:28 +0000

    Updating conf.py to hopefully play nice with rtfd.org

--- guide/source/conf.py
+++ guide/source/conf.py
@@ -110,7 +110,10 @@ pygments_style = 'sphinx'
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
+if on_rtd:
+    html_theme = 'default'
+else:
+    # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
