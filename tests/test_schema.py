6185f5239a5ca335d99e6c2f7a5a95dd405849a3
Noel Merket <noel.merket@nrel.gov>
2022-06-29 15:17:52 -0600

    updating version and fixing guide

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -3,7 +3,8 @@ import pathlib
 import pytest
 
 here = pathlib.Path(__file__).resolve().parent
-example_files = (here.parent / 'examples').glob('*.xml')
+example_files = list((here.parent / 'examples').glob('*.xml'))
+example_files.extend((here.parent / 'guide').glob('**/*.xml'))
 
 
 def test_schema():
@@ -11,9 +12,10 @@ def test_schema():
     xmlschema_doc = etree.parse(str(here / '..' / 'schemas' / 'HPXML.xsd'))
     xmlschema = etree.XMLSchema(xmlschema_doc)  # noqa: F841
 
+
 @pytest.mark.parametrize('filename', example_files, ids=lambda x: x.stem)
 def test_example_file(filename):
     xmlschema_doc = etree.parse(str(here / '..' / 'schemas' / 'HPXML.xsd'))
-    xmlschema = etree.XMLSchema(xmlschema_doc)  # noqa: F841
+    xmlschema = etree.XMLSchema(xmlschema_doc)
     doc = etree.parse(str(filename))
     assert xmlschema.validate(doc)
