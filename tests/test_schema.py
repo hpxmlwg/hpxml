e52f61643b30096877007080ff061aeaf4888b8f
Scott Horowitz <scott.horowitz@nrel.gov>
2023-02-06 09:13:40 -0700

    Update tests; add test for invalid HPXML too.

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -9,13 +9,18 @@ example_files.extend((here.parent / 'guide').glob('**/*.xml'))
 
 def test_schema():
     # Checks that the schema is valid xsd
-    xmlschema_doc = etree.parse(str(here / '..' / 'schemas' / 'HPXML.xsd'))
-    xmlschema = etree.XMLSchema(xmlschema_doc)  # noqa: F841
+    for xsd in ('HPXML.xsd', 'HPXMLCombined.xsd'):
+        xmlschema_doc = etree.parse(str(here / '..' / 'schemas' / xsd))
+        xmlschema = etree.XMLSchema(xmlschema_doc)  # noqa: F841
 
 
 @pytest.mark.parametrize('filename', example_files, ids=lambda x: x.stem)
 def test_example_file(filename):
-    xmlschema_doc = etree.parse(str(here / '..' / 'schemas' / 'HPXML.xsd'))
-    xmlschema = etree.XMLSchema(xmlschema_doc)
-    doc = etree.parse(str(filename))
-    assert xmlschema.validate(doc)
+    for xsd in ('HPXML.xsd', 'HPXMLCombined.xsd'):
+        xmlschema_doc = etree.parse(str(here / '..' / 'schemas' / xsd))
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+        doc = etree.parse(str(filename))
+        if str(filename).endswith('invalid.xml'):
+            assert not xmlschema.validate(doc)
+        else:
+            assert xmlschema.validate(doc)
